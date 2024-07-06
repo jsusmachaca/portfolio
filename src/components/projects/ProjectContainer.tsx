8ec4357a25b9f0b6918db743e3e6b0e80db75378
jsusmachaca <falcorgd@gmail.com>
2024-07-06 11:20:27 -0500

    Change env variable

--- src/components/projects/ProjectContainer.tsx
+++ src/components/projects/ProjectContainer.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react"
 import ProjectItem from "./ProjectItem"
+import axios from "axios"
 
 interface GithubResponse {
   id: number
@@ -16,10 +17,11 @@ interface GithubResponse {
   forks: number
 }
 
-const API_KEY = import.meta.env.API_KEY
+const API_KEY = import.meta.env.PUBLIC_API_KEY
 
 const ProjectContainer = () => {
   const [projects, setProjects] = useState<GithubResponse[]>([])
+  const [isLoading, setIsLoading] = useState(true)
 
   const getIcon = (language: string) => {
     if (!language) return ''
@@ -49,45 +51,49 @@ const ProjectContainer = () => {
     }
   }
 
-  useEffect(() => {
-    fetch('https://api.github.com/user/repos?visibility=public&direction=desc&sort=pushed&affiliation=owner', {
-      headers: {
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
-        'Authorization': `Bearer ${API_KEY}`
-      }
-    })
-      .then (res => {
-        console.log('bro?')
-        console.log(res)
-        return res.json()
+  const fetchProjectData = async () => {
+    try {
+      const response = await axios.get('https://api.github.com/user/repos?visibility=public&direction=desc&sort=pushed&affiliation=owner', {
+        headers: {
+          'Accept': 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28',
+          'Authorization': `Bearer ${API_KEY}`
+        }
       })
-      .then (json => {
-        const filteredProjects = json.filter((project: GithubResponse) => project.language)
-        const projectsWithIcons = filteredProjects.map((project: GithubResponse) => ({
-            ...project,
-            icon: getIcon(project.language)
-          }))
-          setProjects(projectsWithIcons)
-        })
-      .catch (err => console.error(err))
+      const filteredProjects = response.data.filter((project: GithubResponse) => project.language)
+      const projectsWithIcons = filteredProjects.map((project: GithubResponse) => ({
+        ...project,
+        icon: getIcon(project.language)
+      }))
+      setProjects(projectsWithIcons)
+      setIsLoading(false)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  useEffect(() => {
+    fetchProjectData()
   }, [])
 
   return (
     <article id="project-container">
-      { projects.map((item, index) => (
-          <ProjectItem 
-            index={index} 
-            description={item.description} 
-            name={item.name} 
-            html_url={item.html_url} 
-            language={item.language} 
-            icon={item.icon} 
-            homepage={item.homepage}
-            stargazers_count={item.stargazers_count} 
-            forks={item.forks}
-          />
-        ))
+      { isLoading ?
+          <h1 id="load-text">Querying Github repositories ...</h1>
+        :
+          projects.map((item, index) => (
+            <ProjectItem 
+              key={index}
+              index={index} 
+              description={item.description} 
+              name={item.name} 
+              html_url={item.html_url} 
+              language={item.language} 
+              icon={item.icon} 
+              homepage={item.homepage}
+              stargazers_count={item.stargazers_count} 
+              forks={item.forks}
+            />
+          ))
       }
     </article>
   )
