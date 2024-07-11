40f61c7e35a031351229d7542e3e32e6ed4ae183
jsusmachaca <falcorgd@gmail.com>
2024-07-11 18:06:30 -0500

    chore(ProjectItem.tsx): add target='_blank' to external links

--- src/components/projects/ProjectItem.tsx
+++ src/components/projects/ProjectItem.tsx
@@ -20,9 +20,9 @@ const ProjectItem = ({ index, name, description, html_url, homepage, language, i
         <span className="icon-container"><span className="fork-icon"></span>{ forks }</span>
 
         { homepage &&(
-          <a className="urls-button" href={ homepage }><span className="urls-icon">󰖟 </span>Website</a>
+          <a className="urls-button" href={ homepage } target='_blank'><span className="urls-icon">󰖟 </span>Website</a>
         )}
-        <a className="urls-button" href={ html_url }><span className="urls-icon">󰊤 </span>Github Code</a>
+        <a className="urls-button" href={ html_url } target='_blank'><span className="urls-icon">󰊤 </span>Github Code</a>
       </div>
     </div>
   )
